c6f7cdf35276a962140dce9994666d79327c4cfa
rlouvrie <rlouvrie@student.42.fr>
2024-09-13 15:26:55 +0200

    adding argo cd install script for bonus app

---
+++ bonus/scripts/argo-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DOMAIN="gitlab-webservice-default.gitlab.svc.cluster.local:8181"
+
+REPO="pbeheyt42.iot.fr"
+
+USER="root"
+
+APP="playground-gitlab"
+
+argocd app create $APP \
+    --repo "http://${DOMAIN}/${USER}/${REPO}.git" \
+    --path . \
+    --dest-server https://kubernetes.default.svc \
+    --dest-namespace dev \
+    --sync-policy auto
