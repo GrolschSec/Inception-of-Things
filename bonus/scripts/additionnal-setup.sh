09586b0f3e9855af36f5a78d4e535ef72be6fa59
Romain LOUVRIER <rlouvrie@made-f0Ar7s6.clusters.42paris.fr>
2024-09-27 20:54:03 +0200

    updating script

--- bonus/scripts/additionnal-setup.sh
+++ bonus/scripts/additionnal-setup.sh
@@ -6,6 +6,78 @@ IP="127.0.0.1"
 
 HOST="$IP gitlab.$DOMAIN"
 
+echo_color() {
+  NC='\033[0m'
+
+  echo -e "$1$2$NC"
+}
+
+warning() {
+  YELLOW=$(tput setaf 3)
+
+  echo_color "$YELLOW" "[Warning] - $1"
+}
+
+success() {
+  GREEN=$(tput setaf 2)
+  BOLD=$(tput bold)
+
+  echo_color "$BOLD$GREEN" "[Success] - $1"
+}
+
+error() {
+  RED=$(tput setaf 1)
+  BOLD=$(tput bold)
+
+  echo_color "$BOLD$RED" "[Error] - $1"
+}
+
+info() {
+  BLUE=$(tput setaf 4)
+
+  echo_color "$BLUE" "[Info] - $1"
+}
+
+run_cmd() {
+  local command="$1"
+  local info_msg="$2"
+  local success_msg="$3"
+  local error_msg="$4"
+  local use_sudo=${5:-0}
+  local warning_msg="$6"
+  local exit_on_fail=${7:-1}
+
+  # Show the info msg if there is one
+  if [ -n "$info_msg" ]; then
+    info "$info_msg"
+  fi
+
+  # Run the command
+  if [ "$use_sudo" -eq 1 ]; then
+    command="sudo $command"
+    eval "$command" 2> /dev/null
+  else
+    eval "$command" > /dev/null 2>&1
+  fi
+
+  if [ $? -eq 0 ]; then
+    if [ -n "$success_msg" ]; then
+      success "$success_msg"
+    fi
+  else
+    error "$error_msg"
+    if [ "$exit_on_fail" -eq 1 ]; then
+      exit 1
+    fi
+  fi
+
+  if [ -n "$warning_msg" ]; then
+    warning "$warning_msg"
+  fi
+}
+
+clear
+
 GITLAB_PASSWORD=$(kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o jsonpath="{.data.password}" | base64 --decode)
 
 # Adding domain to /etc/hosts
